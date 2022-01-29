207fc0d90f2e3471b6fe93c1bae75adca63cc263
Dev2021 <jmorreale@users.noreply.github.com>
2022-01-29 18:36:23 -0500

    removed comments

--- ssg.py
+++ ssg.py
@@ -3,14 +3,10 @@ from ssg.site import Site
 
 
 def main(source="content", dest="dist"):
-
-    # configuration dictionary
     config = {
         "source": source,
         "dest": dest
     }
-
-    # Unpack dictionary with **
     Site(**config).build()
 
 
--- ssg/site.py
+++ ssg/site.py
@@ -3,7 +3,6 @@ from pathlib import Path
 
 class Site:
     def __init__(self, source, dest):
-        # instantiate Path class with source and dest
         self.source = Path(source)
         self.dest = Path(dest)
 
@@ -16,8 +15,6 @@ def create_dir(self, path):
 def build(self):
     self.dest.mkdir(parents=True, exists_ok=True)
     for path in self.source.rglob("*"):
-
-        # test if the current path is a dir
         if path.is_dir():
             self.create_dir(path)
 

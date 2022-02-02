8179d8fb807f085a076f3d793a5a107029c5033d
Dev2021 <jmorreale@users.noreply.github.com>
2022-02-02 12:34:01 -0500

    fix

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -22,7 +22,7 @@ class Parser:
             file.write(content)
 
     def copy(self, path, source, dest):
-        shutil.copy2(path, self.dest / path.relative_to(self.source))
+        shutil.copy2(path, dest / path.relative_to(source))
 
 
 class ResourceParser(Parser):

839cc3d23c9401fb294389a79367aefa6fa3bffe
Dev2021 <jmorreale@users.noreply.github.com>
2022-02-02 13:37:06 -0500

    fixes

--- ssg/site.py
+++ ssg/site.py
@@ -12,14 +12,13 @@ class Site:
         directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
 
+    def load_parser(self, extension):
+        for parser in self.parsers:
+            if parser.valid_extension():
+                return parser
+
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
-
-    def load_parser(self, extension):
-        for self.parser in self.parsers:
-            if extension.valid_extension():
-                return self.parser
-            

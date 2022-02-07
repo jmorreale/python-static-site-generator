7ef20ec2e058ef0bfe610c7e3d0c0086fc9e85b8
Dev2021 <jmorreale@users.noreply.github.com>
2022-02-06 21:16:24 -0500

    more content

--- ssg/content.py
+++ ssg/content.py
@@ -1,9 +1,23 @@
 import re
-from abc import ABC
 
 from yaml import load, FullLoader
-from _collections_abc import Mapping
+from collections.abc import Mapping
 
 
-class Content(Mapping, ABC):
-    __delimeter = '^(?:-|\+){3}\s*$'
+class Content(Mapping ):
+    __delimeter = r"^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimeter, re.MULTILINE)
+
+    @classmethod
+    def load( cls, string):
+        _, fm, content = cls.__regex.split(string, 2)
+        metadata = load(fm, Loader=FullLoader)
+        return cls(metadata, content)
+
+    def __init__(self, metadata, content):
+        self.data = metadata
+        self.data ["content"] = content
+
+    @property
+    def body(self):
+        return self.data["content"]

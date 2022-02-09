33da3d467a284833be0201667e8e30eea57ced8e
Dev2021 <jmorreale@users.noreply.github.com>
2022-02-09 12:13:47 -0500

    fixes for content class

--- ssg/content.py
+++ ssg/content.py
@@ -21,3 +21,29 @@ class Content(Mapping ):
     @property
     def body(self):
         return self.data["content"]
+
+    @property
+    def type(self):
+        return self.data["type"] if "type" in self.data else None
+
+    @type.setter
+    def type(self):
+        self.data["type"] = type
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __iter__(self):
+        self.data.__iter__()
+
+    def __len__(self):
+        return len(self.data)
+
+    def __repr__(self):
+        data = {}
+        for key, value in self.data.items():
+            if key != "content":
+             data[key] = value
+        return str(data)
+
+
